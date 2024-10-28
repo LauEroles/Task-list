a6c5344dd336a149de625574ceb69fbd8bff1a45
AdrielReina <adriel.a.reina@gmail.com>
2024-10-27 21:38:39 -0300

    Class Estadistica y Enums
    Se genero la clase estadistica y los enums iniciales del diagrama.

---
+++ src/Clases/Estadistica.ts
@@ -0,0 +1,21 @@
+//agregar import de clase tarea cuando este creado.
+
+export default class Estadistica{
+    
+    public tareasCompletadas(tareas : Tarea[]) :void {
+
+    }
+
+    public TareasPendientes(tareas : Tarea[]) :void{
+
+    }
+
+
+    public tazaFinalizacionTarea(tarea : Tarea) :number{
+
+    }
+
+    public tiempoDedicadoATarea(tarea : Tarea) :number{
+
+    }
+}
