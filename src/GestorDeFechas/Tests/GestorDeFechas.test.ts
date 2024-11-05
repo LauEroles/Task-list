48d7c0d0f42b1db6a30ca1b99ea71610c9290942
AdrielReina <adriel.a.reina@gmail.com>
2024-11-04 22:06:16 -0300

    test a desarrollar Gestor fechas
    estructura base test Gestor Fecha

--- src/GestorDeFechas/Tests/GestorDeFechas.test.ts
+++ src/GestorDeFechas/Tests/GestorDeFechas.test.ts
@@ -1,6 +1,62 @@
 import GestorDeFechas from "../GestorDeFechas"
+import {mock} from "jest-mock-extended";
 
-describe ("Tests clase Estadistica", () => { 
+describe ("Tests clase GestorDeFecha", () => { 
 
+    let gestorDeFechas : GestorDeFechas; 
+
+    beforeEach(() =>{   
     
+        gestorDeFechas = new GestorDeFechas();
+     
+    })
+
+    test ("capturar Fecha actual correcta", ()=>{
+
+      expect(gestorDeFechas.capturarFechaActual())toEqual.(new Date(Date.now()));
+
+    })
+
+    test ("capturar dia Correcto", ()=>{
+
+        expect(gestorDeFechas.())toEqual.();
+  
+    })
+
+    test ("capturar mes Correcto", ()=>{
+
+        expect(gestorDeFechas.())toEqual.();
+  
+    })
+
+    test ("capturar año Correcto", ()=>{
+
+        expect(gestorDeFechas.())toEqual.();
+  
+    })
+
+    test ("capturar tiempo Transcurrido En Milisegundos Correcto", ()=>{
+
+        expect(gestorDeFechas.())toEqual.();
+  
+    })
+
+    test ("capturar fecha nueva Correcta", ()=>{
+
+        expect(gestorDeFechas.())toEqual.();
+  
+    })
+
 }
+
+/*
++diaFecha(fecha:Date):Number
++mesFecha(fecha:Date):Number
++añoFecha(fecha:Date):Number
++capturarFechaActual():Date
++nuevaFecha(dia:number,mes:number,año:number):Date
++tiempoTranscurridoEnMilisegundos(fechaInicio:Date):number
+
+//+Funciones De conversiones
+milisegundoA
+*/
