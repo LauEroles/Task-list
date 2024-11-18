a7c5505b901c0c325fdc91974dfdfd654af2dff4
Laura Eroles <laura.eroles@gmail.com>
2024-11-18 20:24:45 -0300

    Cambios en clase App y Estadistica

--- src/Estadistica/Estadistica.ts
+++ src/Estadistica/Estadistica.ts
@@ -7,28 +7,26 @@ export default class Estadistica{
 
     private gestorDeFecha : GestorDeFechas = new GestorDeFechas();
 
-    public tareasCompletadas(tareas : Tarea[]) :Tarea[] {
 
+    public tareasCompletadas(tareas : Tarea[]) :Tarea[] {
         return this.filtrarPorEstadoTareas(tareas,EstadoTarea.COMPLETADO);
     }
 
-    public TareasPendientes(tareas : Tarea[]) :Tarea[]{
 
+    public TareasPendientes(tareas : Tarea[]) :Tarea[]{
         return this.filtrarPorEstadoTareas(tareas,EstadoTarea.PENDIENTE);
     }
 
-    private filtrarPorEstadoTareas (tareas: Tarea[],filtro : string) : Tarea[] { //revisitar
-        
-        let tareasFiltradas : Tarea[] = 
-        
-        tareas.filter((tarea)=> {
-            tarea.getEstado() === filtro;
+
+    private filtrarPorEstadoTareas (tareas: Tarea[],estado :EstadoTarea) : Tarea[] { //revisitar
+        let tareasFiltradas : Tarea[] = tareas.filter((tarea)=> {
+            tarea.getEstadoTarea() === estado;
         });
 
         return tareasFiltradas;
     }
     
-    public tazaFinalizacionTareas(tareas : Tarea[]) : number{   // % Cant tareas finalizadas
+    public tasaFinalizacionTareas(tareas : Tarea[]) : number{   // % Cant tareas finalizadas
         
         let cantidadTareas : number = tareas.length;
         let cantidadFinalizada : number = this.tareasCompletadas(tareas).length;
@@ -36,6 +34,7 @@ export default class Estadistica{
         return cantidadTareas / cantidadFinalizada; //agregar try catch por division 0?
     }
 
+    /* 
     public tiempoDedicadoATareas(tareas : Tarea[]) : number{
         let tiempoDedicado : number = 0;
 
@@ -46,6 +45,8 @@ export default class Estadistica{
         return tiempoDedicado;
     }
 
+    VER BIEN ESTO PORQUE EL TIEMPO TRANSCURRIDO NO ES IGUAL AL TIEMPO DEDICADO, PUEDE PASAR UN AÑO Y NO HABERLE DEDICADO TIEMPO A LA TAREA
+
     public tiempoDedicadoATarea(tarea : Tarea) : number{
 
         let fechaInicio : Date = tarea.getFechaInicio();
@@ -54,5 +55,5 @@ export default class Estadistica{
 
         return tiempoDedicado;
     }
-
+    */
 }
